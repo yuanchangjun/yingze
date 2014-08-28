839a0486e3fad78545843d57607b6e9ac86ee5ad
yuanchangjun <qinxiangdexunyicao@gmail.com>
2014-08-28 17:40:44 +0800

    重构了DispatchServletTest类

--- src/test/java/com/baldurtech/DispatchServletTest.java
+++ src/test/java/com/baldurtech/DispatchServletTest.java
@@ -3,8 +3,14 @@ package com.baldurtech;
 import com.baldurtech.unit.TestCase;
 
 public class DispatchServletTest extends TestCase{
-    public void test_uri_contact_show_应该由ContactAction来处理(){
-        DispatchServlet dispatchServlet = new DispatchServlet();
-        assertEquals("com.baldurtech.ContactAction",dispatchServlet.getActionClassNameByUri("/contact/show"));
+    
+    DispatchServlet dispatchServlet = new DispatchServlet();
+    String actionClassName = "com.baldurtech.ContactAction";
+    public void test_uri_contact_show_应该由ContactAction来处理(){       
+        assertEquals(actionClassName,dispatchServlet.getActionClassNameByUri("/contact/show"));
+    }
+
+    public void test_uri_contact_应该由ContactAction来处理(){       
+        assertEquals(actionClassName,dispatchServlet.getActionClassNameByUri("/contact"));
     }
 } 
