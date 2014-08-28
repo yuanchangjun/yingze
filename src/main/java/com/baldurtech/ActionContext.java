e2e36c6b91845021d57d7dc0820c46fbc73762f6
yuanchangjun <qinxiangdexunyicao@gmail.com>
2014-08-29 00:24:54 +0800

    添加了ActionContext接口

---
+++ src/main/java/com/baldurtech/ActionContext.java
@@ -0,0 +1,9 @@
+package com.baldurtech;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface ActionContext{
+    
+}
