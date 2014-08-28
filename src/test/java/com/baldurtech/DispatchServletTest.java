b3a6bbc4e7c736b25d86f1e16c75c6ca7df26343
yuanchangjun <qinxiangdexunyicao@gmail.com>
2014-08-28 23:12:12 +0800

    修改测试，给方法后面都加上默认的后缀.jsp，有测试失败

--- src/test/java/com/baldurtech/DispatchServletTest.java
+++ src/test/java/com/baldurtech/DispatchServletTest.java
@@ -7,14 +7,14 @@ public class DispatchServletTest extends TestCase{
     DispatchServlet dispatchServlet = new DispatchServlet();
     String actionClassName = "com.baldurtech.ContactAction";
     public void test_uri_contact_show_应该由ContactAction来处理(){       
-        assertEquals(actionClassName,dispatchServlet.getActionClassNameByUri("/contact/show"));
+        assertEquals(actionClassName,dispatchServlet.getActionClassNameByUri("/contact/show.jsp"));
     }
 
     public void test_uri_contact_应该由ContactAction来处理(){       
         assertEquals(actionClassName,dispatchServlet.getActionClassNameByUri("/contact"));
     }
     public void test_uri_contact_show_的处理方法是show(){
-        assertEquals("show",dispatchServlet.getActionMethodNameByUri("/contact/show"));
+        assertEquals("show",dispatchServlet.getActionMethodNameByUri("/contact/show.jsp"));
     }
     public void test_uri_contact_的默认处理方法是index(){
         assertEquals("index",dispatchServlet.getActionMethodNameByUri("/contact"));
